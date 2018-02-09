340653d7c1158a5fa42bcde6f7dccdc65e8ad194
384401056 <384401056@qq.com>
2018-02-10 00:04:34 +0800

    custom-global-components

---
+++ custom-global-components/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="login">
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return{
+				msg:'用户登录'
+			}
+		}
+	}
+</script>
+
+<style>
+	#login{
+		color:red;
+	}	
+</style>
